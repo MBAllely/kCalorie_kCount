3b1c0da92c7ef7bf54aca85d1a3e30fb69f8f471
Marika <mballely@gmail.com>
2016-03-25 14:49:45 -0700

    move html in edit-meal.component.ts to edit-meal.component.html

--- app/edit-meal.component.ts
+++ app/edit-meal.component.ts
@@ -5,19 +5,7 @@ import { MealComponent } from './meal.component';
 @Component({
   selector: 'edit-meal',
   inputs: ['meal'],
-  template: `
-    <div class="meal-form form-inline">
-      <div class="details-display">
-        <h2>{{ meal.name }}</h2>
-        <h5> Details: {{ meal.details }}</h5>
-        <h5> Calories: {{ meal.calories }}</h5>
-      </div>
-      <h3>Edit Meal: </h3>
-      <input [(ngModel)]="meal.name" type="text" class="form-control input-lg">
-      <input [(ngModel)]="meal.details" type="text" class="form-control input-lg">
-      <input [(ngModel)]="meal.calories" type="number" class="form-control input-lg">
-    </div>
-  `
+  templateUrl: 'app/edit-meal.component.html' 
 })
 
 export class EditMealComponent {
