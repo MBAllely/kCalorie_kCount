8fa7d57db55b07d873aa0753d8d5cb3f8eb5f553
Marika <mballely@gmail.com>
2016-03-25 10:47:03 -0700

    rename calories.pipe to high-calories.pipe

---
+++ app/calories.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Pipe({
+  name: "calories",
+  pure: false
+})
+
+export class CaloriesPipe implements PipeTransform {
+  transform(input: Meal) {
+    return input.getCalories() > 300;
+  }
+}
--- app/meal.model.ts
+++ app/meal.model.ts
@@ -2,4 +2,7 @@ export class Meal {
   constructor(public name: string, public details: string, public calories: number){
 
   }
+  getCalories() {
+    return this.calories;
+  }
 }
